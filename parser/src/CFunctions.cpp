963bcab12660301bcb605717c6ace7728a494117
Robert Smeets <robertsmeets@gmail.com>
2015-02-25 12:00:00 +0100

    2nd commit attempt

---
+++ parser/src/CFunctions.cpp
@@ -0,0 +1,153 @@
+/*
+ * CFunctions.cpp
+ *
+ *  Created on: Feb 25, 2015
+ *      Author: robert
+ */
+
+#include "CFunctions.h"
+
+unsigned int func_plus_ii(unsigned int i, unsigned int j) {
+	return i + j;
+}
+
+double func_plus_id(int i, double j) {
+	return i + j;
+}
+
+double func_plus_di(double i, int j) {
+	return i + j;
+}
+
+double func_plus_dd(double i, double j) {
+	return i + j;
+}
+
+unsigned int func_minus_ii(unsigned int i, unsigned int j) {
+	return i - j;
+}
+
+double func_minus_id(int i, double j) {
+	return i - j;
+}
+
+double func_minus_di(double i, int j) {
+	return i - j;
+}
+
+double func_minus_dd(double i, double j) {
+	return i - j;
+}
+
+unsigned int func_mul_ii(unsigned int i, unsigned int j) {
+	return i * j;
+}
+
+double func_mul_id(int i, double j) {
+	return i * j;
+}
+
+double func_mul_di(double i, int j) {
+	return i * j;
+}
+
+double func_mul_dd(double i, double j) {
+	return i * j;
+}
+
+bool func_eq_ii(unsigned int i, unsigned int j) {
+	return i == j;
+}
+
+bool func_eq_id(int i, double j) {
+	return i == j;
+}
+
+bool func_eq_di(double i, int j) {
+	return i == j;
+}
+
+bool func_eq_dd(double i, double j) {
+	return i == j;
+}
+
+bool func_ne_ii(unsigned int i, unsigned int j) {
+	return i != j;
+}
+
+bool func_ne_id(unsigned int i, double j) {
+	return i != j;
+}
+
+bool func_ne_di(double i, unsigned int j) {
+	return i != j;
+}
+
+bool func_ne_dd(double i, double j) {
+	return i != j;
+}
+
+bool func_lt_ii(unsigned int i, unsigned int j) {
+	return i < j;
+}
+
+bool func_lt_id(unsigned int i, double j) {
+	return i < j;
+}
+
+bool func_lt_di(double i, int j) {
+	return i < j;
+}
+
+bool func_lt_dd(double i, double j) {
+	return i < j;
+}
+
+bool func_ge_ii(unsigned int i, unsigned int j) {
+	return i >= j;
+}
+
+bool func_ge_id(unsigned int i, double j) {
+	return i >= j;
+}
+
+bool func_ge_di(double i, unsigned int j) {
+	return i >= j;
+}
+
+bool func_ge_dd(double i, double j) {
+	return i >= j;
+}
+
+bool func_gt_ii(unsigned int i, unsigned int j) {
+	return i > j;
+}
+
+bool func_gt_id(unsigned int i, double j) {
+	return i > j;
+}
+
+bool func_gt_di(double i, unsigned int j) {
+	return i > j;
+}
+
+bool func_gt_dd(double i, double j) {
+	return i > j;
+}
+
+bool func_le_ii(unsigned int i, unsigned int j) {
+	return i <= j;
+}
+
+bool func_le_id(unsigned int i, double j) {
+	return i <= j;
+}
+
+bool func_le_di(double i, unsigned int j) {
+	return i <= j;
+}
+
+bool func_le_dd(double i, double j) {
+	return i <= j;
+}
+
