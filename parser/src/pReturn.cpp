de232d6c4114599e66477c24d473df238d71a865
Robert Smeets <robertsmeets@gmail.com>
2016-01-25 22:00:10 -0700

    return parameter works

--- parser/src/pReturn.cpp
+++ parser/src/pReturn.cpp
@@ -9,19 +9,27 @@ pReturn::~pReturn() {
 }
 
 void pReturn::print(int level) {
-	for (unsigned int i = 0; i < level; i++) {
-		cout << "+";
-	}
-	cout << "Return " << endl;
-	expression->print(level+1);
+   for (unsigned int i = 0; i < level; i++) {
+      cout << "+";
+   }
+   cout << "Return " << endl;
+   expression->print(level+1);
 }
 
 void pReturn::emit(CodeGenerator* cg, pProcedureNode* pn)
 {
-	//
-		// emit RET
-		//
-		cg->emit(2, 0, 0, NULL);
+   //
+   // emit RET
+   //
+   if (expression != NULL)
+   {
+      expression->emit(cg,pn);
+      cg->emit(2, 1, 0, NULL);
+   }
+   else
+   {
+      cg->emit(2,0,0,NULL);
+   }
 }
 
 string pReturn::stype() {return "pReturn";}
