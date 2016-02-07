7d160ecdcfb81d553523912b5efb752dafd9344d
Robert Smeets <robertsmeets@gmail.com>
2016-02-06 22:32:09 -0700

    improvements in parameter handling

--- src/CInterpreter.h
+++ src/CInterpreter.h
@@ -17,6 +17,7 @@
 //#include <dyncall_callf.h>
 //#include <dynload.h>
 #include <math.h>
+#include "stdint.h"
 
 #include "HeapManager.h"
 #include "PException.h"
@@ -28,48 +29,48 @@ using namespace std;
 class HeapManager; // forward declaration
 
 struct stack_element {
-		unsigned short int atype;
-		unsigned short int address;
-	};
+      unsigned short int atype;
+      unsigned short int address;
+   };
 
 class CInterpreter {
-	DebugInfo* di;
-	char* buffer;
-	uint16_t pc;
-	uint16_t t;
-	uint16_t tr;
-	uint16_t tb;
-	vector<stack_element> s;
-	vector<unsigned short int> r;
-	vector<unsigned short int> b;
-	HeapManager* hm;
+   DebugInfo* di;
+   char* buffer;
+   uint16_t pc;
+   uint16_t t;
+   uint16_t tr;
+   uint16_t tb;
+   vector<stack_element> s;
+   vector<unsigned short int> r;
+   vector<unsigned short int> b;
+   HeapManager* hm;
 
-	typedef uint16_t (*iiptr)(uint16_t, uint16_t);
-	typedef double (*ddptr)(double, double);
-	typedef double (*idptr)(uint16_t, double);
-	typedef double (*diptr)(double, uint16_t);
+   typedef uint16_t (*iiptr)(uint16_t, uint16_t);
+   typedef double (*ddptr)(double, double);
+   typedef double (*idptr)(uint16_t, double);
+   typedef double (*diptr)(double, uint16_t);
 
-	typedef bool (*biiptr)(uint16_t, uint16_t);
-	typedef bool (*bddptr)(double, double);
-	typedef bool (*bidptr)(uint16_t, double);
-	typedef bool (*bdiptr)(double, uint16_t);
+   typedef bool (*biiptr)(uint16_t, uint16_t);
+   typedef bool (*bddptr)(double, double);
+   typedef bool (*bidptr)(uint16_t, double);
+   typedef bool (*bdiptr)(double, uint16_t);
 
-	iiptr fptrs[14][8][8];
+   iiptr fptrs[14][8][8];
 
-	map<uint16_t,map<uint16_t, uint16_t[3]>> methodmap;
+   map<uint16_t,map<uint16_t, uint16_t[3]>> methodmap;
 
 
 public:
-	CInterpreter(char*, DebugInfo*);
-	virtual ~CInterpreter();
-	void start();
-	int execute_next();
-	void print_a_string(char*);
-	void print_a_string(char*, uint16_t);
-	void call_external(char*, uint16_t);
-	vector<stack_element>* getStack();
-	void check_magic_number();
-	uint16_t find_offset();
+   CInterpreter(char*, DebugInfo*);
+   virtual ~CInterpreter();
+   void start();
+   int execute_next();
+   void print_a_string(char*,bool);
+   void print_a_string(char*, uint16_t);
+   void call_external(char*, uint16_t);
+   vector<stack_element>* getStack();
+   void check_magic_number();
+   uint16_t find_offset();
 };
 
 #endif /* INTERPRETER_SRC_INTERPRETER_H_ */
