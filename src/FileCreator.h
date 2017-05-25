385f8be399fe0d993e56ff1e62749d4584039061
Robert Smeets <robertsmeets@gmail.com>
2017-05-24 18:11:53 -0700

    create file

--- src/FileCreator.h
+++ src/FileCreator.h
@@ -1,12 +1,14 @@
 #ifndef FILECREATOR_H_
 #define FILECREATOR_H_ 
 
+#include <iostream>
+
 using namespace std;
 
 class FileCreator {
 
 public:
-   void start(char* ,char* , int );
+   void start(char* ,char*, char* , int );
 };
 #endif
 
