69eb7c9f46b5de2281cc92fa1712da781e013b59
robertsmeets <robertsmeets@gmail.com>
2017-07-05 23:45:57 -0700

    Changed .exe file handling

--- src/SelfStart.h
+++ src/SelfStart.h
@@ -6,12 +6,25 @@
 #include "gc.h"
 #include <string.h>
 #include "CInterpreter.h"
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <string>
+
+#ifdef _WIN32
+   #define XMAGIC "MZ"
+#else
+   #define XMAGIC "\x7F""ELF" 
+#endif
 
 using namespace std;
+
 
 class SelfStart {
 
 public:
    int start(char*);
+   string shorten(char*);
 };
 #endif
+
