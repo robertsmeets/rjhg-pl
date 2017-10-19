2089be3eb44a271c079b1cb7842366b32adbc611
robertsmeets <robertsmeets@gmail.com>
2017-10-19 07:09:37 -0700

    Some C conversions

---
+++ src/CFunctions.c
@@ -0,0 +1,177 @@
+/*
+ * CFunctions.cpp
+ *
+ *  Created on: Feb 25, 2015
+ *      Author: robert
+ */
+
+#include "CFunctions.h"
+
+unsigned int func_plus_ii(unsigned int i, unsigned int j) { return i + j; }
+double func_plus_id(int i, double j) { return i + j; } 
+double func_plus_di(double i, int j) { return i + j; }
+double func_plus_dd(double i, double j) { return i + j; }
+unsigned int func_minus_ii(unsigned int i, unsigned int j) { return i - j; }
+double func_minus_id(int i, double j) { return i - j; }
+double func_minus_di(double i, int j) { return i - j; }
+double func_minus_dd(double i, double j) { return i - j; }
+unsigned int func_mul_ii(unsigned int i, unsigned int j) { return i * j; }
+double func_mul_id(int i, double j) { return i * j; }
+double func_mul_di(double i, int j) { return i * j; }
+double func_mul_dd(double i, double j) { return i * j; }
+bool func_eq_nn(unsigned int i, unsigned int j) { return true ; }
+bool func_eq_ii(unsigned int i, unsigned int j) { return i == j; }
+bool func_eq_id(int i, double j) { return i == j; }
+bool func_eq_di(double i, int j) { return i == j; }
+bool func_eq_dd(double i, double j) { return i == j; }
+bool func_eq_on(unsigned int i, unsigned int j) { return false; }
+bool func_eq_no(unsigned int i, unsigned int j) { return false; }
+bool func_ne_nn(unsigned int i, unsigned int j) { return false ; }
+bool func_ne_ii(unsigned int i, unsigned int j) { return i != j; }
+bool func_ne_id(unsigned int i, double j) { return i != j; }
+bool func_ne_di(double i, unsigned int j) { return i != j; }
+bool func_ne_dd(double i, double j) { return i != j; }
+bool func_lt_ii(unsigned int i, unsigned int j) { return i < j; }
+bool func_lt_id(unsigned int i, double j) { return i < j; }
+bool func_lt_di(double i, int j) { return i < j; }
+bool func_lt_dd(double i, double j) { return i < j; }
+bool func_ge_ii(unsigned int i, unsigned int j) { return i >= j; }
+bool func_ge_id(unsigned int i, double j) { return i >= j; }
+bool func_ge_di(double i, unsigned int j) { return i >= j; }
+bool func_ge_dd(double i, double j) { return i >= j; }
+bool func_gt_ii(unsigned int i, unsigned int j) { return i > j; }
+bool func_gt_id(unsigned int i, double j) { return i > j; }
+bool func_gt_di(double i, unsigned int j) { return i > j; }
+bool func_gt_dd(double i, double j) { return i > j; }
+bool func_le_ii(unsigned int i, unsigned int j) { return i <= j; }
+bool func_le_id(unsigned int i, double j) { return i <= j; }
+bool func_le_di(double i, unsigned int j) { return i <= j; }
+bool func_le_dd(double i, double j) { return i <= j; }
+
+/**
+ *
+ * return the size of a string
+ *
+ */
+void string_size(char* ptr,struct stack_element* s,uint16_t* t,bool debug) 
+{
+   struct stack_element fr1 = s[*t-1];
+   if (fr1.atype != TYPE_STRING)
+   {
+      printf("size requested, but this is not a string\n");
+      exit(-1);
+   }
+   char * ptr1 = (char*) fr1.address;
+   uint16_t len1 = ((*ptr1) & 0xff) + (*(ptr1 + 1) << 8);
+   s[*t].atype=TYPE_INT;
+   s[*t].address=len1;
+   (*t)++; 
+}
+
+
+/**
+ *
+ * this adds an element to an array 
+ *
+ */
+void array_add(char* ptr,struct stack_element* s,uint16_t* t,bool debug) 
+{
+   if (debug){printf("In the array_add one\n");}
+   //
+   // byte 0 and 1 are the actual length
+   // byte 2 and 3 are the claimed length
+   //
+   uint16_t* uptr = (uint16_t*)  ptr;
+   if (debug){printf("In the array_add two ptr=%p\n",ptr);}
+   uint16_t actual = *uptr ;
+   if (debug){printf("In the array_add three\n");}
+   uptr = (uint16_t*) (ptr+2);
+   if (debug){printf("In the array_add four\n");}
+   uint16_t claimed = *uptr;
+   if (debug){printf("In the array_add five\n");}
+   char* nptr = ptr+4;
+   if (debug){printf("In the array_add six\n");}
+   char** ptrptr = (char**)nptr; 
+   if (debug){printf("In the array_add seven\n");}
+   char* spaceptr =  *ptrptr;
+   if (debug){printf("In the array_add eight\n");}
+   actual++;
+   if (actual > claimed)
+   {
+      //
+      // resize that array
+      //
+      claimed *= 2;
+      if (debug) {printf("resize larger spaceptr = %p actual = %d claimed = %d\n",spaceptr,actual,claimed);};
+      spaceptr = (char*)GC_REALLOC(spaceptr, 8 * claimed);
+      if (debug) {printf("after larger\n");};
+      *ptrptr = spaceptr;
+   }
+   if ((actual > 10) && (actual < claimed / 3))
+   {
+      //
+      // resize that array
+      //
+      claimed /= 2;
+      if (debug) {printf("resize smaller %p actual = %d claimed = %d\n",ptr,actual,8*claimed);};
+      spaceptr = (char*)GC_REALLOC(spaceptr,8 * claimed);
+      if (debug) {printf("after smaller\n");};
+      *ptrptr = spaceptr;
+   }
+   //
+   // set the new size
+   //
+   *ptr = actual & 0xff;
+   *(ptr+1) = actual >> 8;
+   //
+   // set the new claimed value
+   //
+   *(ptr+2) = claimed & 0xff;
+   *(ptr+3) = claimed >> 8;
+   //
+   // set the value
+   //
+   char* mptr = spaceptr + (actual-1) * 8;
+   *mptr = s[*t-2].atype & 0xff;
+   char* avptr = (char*)s[*t-2].address;
+   char** vptr = (char**)(mptr+1);
+   *vptr = avptr; 
+   (*t)--;
+   (*t)--;
+}
+
+void array_set(char* ptr,struct stack_element* s,uint16_t* t,bool debug) 
+{
+   //
+   // byte 0 and 1 are the actual length
+   // byte 2 and 3 are the claimed length
+   //
+   int actual = (*ptr & 0xff) + ((*(ptr + 1) & 0xff) << 8) ;
+   int claimed = (*(ptr+2) & 0xff)  + ((*(ptr + 3) & 0xff) << 8);
+   char* nptr = ptr+4;
+   char** ptrptr = (char**)nptr; 
+   char* spaceptr =  *ptrptr;
+   //
+   // set the value
+   //
+   int atype = s[*t-3].atype & 0xff;
+   if (atype != TYPE_INT)
+   {
+       printf("array index is not an integer\n");
+       exit(-1);
+   }
+   int index = s[*t-3].address;
+   if (index >= actual)
+   {
+       printf("array index %d out of range, array size is %d\n",index,actual);
+       exit(-1);
+   }       
+   char* mptr = spaceptr + index * 8;
+   *mptr = s[*t-2].atype & 0xff;
+   char* avptr = (char*)s[*t-2].address;
+   char** vptr = (char**)(mptr+1);
+   *vptr = avptr; 
+   (*t)--;
+   (*t)--;
+}
+
--- src/CInterpreter.h
+++ src/CInterpreter.h
@@ -8,52 +8,41 @@
 #ifndef INTERPRETER_SRC_INTERPRETER_H_
 #define INTERPRETER_SRC_INTERPRETER_H_
 
-#include <vector>
-#include <map>
 #include <stdio.h>
-#include <cstring>
-#include <sstream>
 #include <dyncall.h>
 #include <dyncall_callf.h>
 #include <dynload.h>
 #include <math.h>
 #include <stdint.h>
 #include <gc.h>
+#include <stdbool.h>
 
-#include "Disassembler.h"
 #include "CConstants.h"
-#include "DebugInfo.h"
-#include "jwHash.hpp"
-
-using namespace std;
-
-class HeapManager; // forward declaration
+#include "jwHash.h"
+#include "CFunctions.h"
 
 struct stack_element {
       unsigned short int atype;
       unsigned long long int address;
 };
-#include "CFunctions.h"
 
 struct extern_record {
    unsigned long long int address;
-   string signature;
+   char* signature;
 };
 
-class CInterpreter {
    bool debug;
-   DebugInfo* di;
    char* buffer;
    uint16_t pc;
    uint16_t t;
    uint16_t tr;
    uint16_t tb;
-   stack_element s[500];
-   vector<unsigned short int> r;
-   vector<unsigned short int> b;
-   HeapManager* hm;
+   struct stack_element s[500];
+   unsigned short int r[500];
+   unsigned short int b[500];
+   // HeapManager* hm;
 
-   typedef void (*especial) (char* ,vector<stack_element>* ,uint16_t* ,bool,CInterpreter* );
+   //typedef void (*especial) (char* ,vector<stack_element>* ,uint16_t* ,bool,CInterpreter* );
    typedef uint16_t (*iiptr)(uint16_t, uint16_t);
    typedef double (*ddptr)(double, double);
    typedef double (*idptr)(uint16_t, double);
@@ -66,22 +55,18 @@ class CInterpreter {
    iiptr fptrs[14][8][8];
 
    jwHashTable* methodmap;
-   vector<extern_record> externs;
+   unsigned int extern_count = 0;
+   struct extern_record externs[100];
    uint16_t find_ext_proc_table(); 
    void check_magic_number();
    uint16_t find_offset();
    void call_external(short unsigned int,short unsigned int) ;
-   void pass_in_arg( DCCallVM* ,char ,stack_element);
-public:
-   CInterpreter(char*, DebugInfo*);
-   virtual ~CInterpreter();
+   void pass_in_arg( DCCallVM* ,char ,struct stack_element);
    void start(bool);
    int execute_next();
    void print_a_string(char*,bool);
-   void print_a_string(char*, uint16_t);
    void print_stack();
-   stack_element* getStack();
+   struct stack_element* getStack();
    int getStackDepth();
-};
 
 #endif /* INTERPRETER_SRC_INTERPRETER_H_ */
