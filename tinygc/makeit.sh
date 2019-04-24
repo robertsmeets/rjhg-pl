9bac9c57ea7cbc6410c8d3734bede40d5d0afcdd
robertsmeets <robertsmeets@gmail.com>
2019-04-23 22:56:35 -0700

    add bdwgc back in

--- tinygc/makeit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#-c means to create an intermediary object file, rather than an executable.
-gcc -c -o tinygc.o tinygc.c -DGC_FASTCALL= -DGC_CLIBDECL=
-#
-#This creates the static library. r means to insert with replacement, c means to create a new archive, and s means to write an index. As always, see the man page for more info.
-#
-ar rcs libgc-lib.a tinygc.o
